705cc23cfb4fbd84a5d4a5c71da44542651cdd06
Nick Collings <nick@collingo.com>
2014-10-29 18:02:45 +0000

    updating npm commands

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static(path.resolve('www')));
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+console.log('App listening on port '+port);
